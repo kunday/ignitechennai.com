4880247a39f3fef9320856db7793b3db3085a359
Arvind Kunday <hi@kunday.com>
2010-03-17 11:45:01 +0530

    Removed phone_no

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,4 +1,3 @@
 class Registration < ActiveRecord::Base
-  validates_presence_of :name, :last_name, :email, :one_line_abstract, :phone_no, :mode_of_reach, :company_name
-  validates_numericality_of :phone_no
+  validates_presence_of :name, :last_name, :email, :one_line_abstract, :mode_of_reach, :company_name
 end
