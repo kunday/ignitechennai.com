b64c0cb5ce11f5dd22ec9c2ec9ce1e26ef82963b
Arvind Kunday <kunday@gmail.com>
2010-04-06 18:15:00 +0530

    Added mandatory changes to the mode of reach

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,3 @@
 class Registration < ActiveRecord::Base
-  validates_presence_of :name, :last_name, :email, :mode_of_reach, :company_name, :type_of_attendee
+  validates_presence_of :name, :last_name, :email, :mode_of_reach, :company_name, :type_of_attendee, :phone_no
 end
