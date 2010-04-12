4b3e6c3bb679eec9817bf517c89e0c483c2a819f
Arvind Kunday <kunday@gmail.com>
2010-04-12 12:30:06 +0530

    Using the new layout and registration page

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '1e5bc663aac87d6935e4d27e51e3e11e'
-  layout 'master'
+  layout 'ignite'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
